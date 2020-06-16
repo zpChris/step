f4e58181f709df8212333dfcb823719163edd1f3
Chris Elliott <elliott77chris@gmail.com>
2020-06-16 00:02:57 +0000

    Get conflicting times from events, and merge conflicting times.

--- walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -14,10 +14,104 @@
 
 package com.google.sps;
 
+import java.util.Collections;
 import java.util.Collection;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Comparator;
 
 public final class FindMeetingQuery {
+
   public Collection<TimeRange> query(Collection<Event> events, MeetingRequest request) {
-    throw new UnsupportedOperationException("TODO: Implement this method.");
+    
+    // This variable will hold all of the invalid meeting times.
+    // This list is sorted by start times, and has no overlapping times.
+    List<TimeRange> invalidTimeRanges = new ArrayList<>();
+    
+    // This is the list of the attendees which attend the meeting.
+    Collection<String> attendees = request.getAttendees();
+
+    // Populate invalidTimeRanges variable, and sort by increasing start time.
+    invalidTimeRanges = getInvalidTimeRanges(invalidTimeRanges, events, attendees);
+    Collections.sort(invalidTimeRanges, new Comparator<TimeRange>() {
+      public int compare(TimeRange t1, TimeRange t2) {
+        if (t1.start() < t2.start()) {
+          return -1;
+        } else if (t1.start() == t2.start()) {
+          return 0;
+        } else {
+          return 1;
+        }
+      }
+    });
+
+    // Merge conflicting TimeRanges.
+    invalidTimeRanges = mergeConflictingTimeRanges(invalidTimeRanges);
+
+    return invalidTimeRanges;
+
   }
+
+  /**
+   * Merge any conflicting TimeRanges in the invalidTimeRanges list.
+   */
+  private List<TimeRange> mergeConflictingTimeRanges(
+    List<TimeRange> invalidTimeRanges) {
+      
+    int index = 0;
+    while (index < invalidTimeRanges.size() - 1) {
+      // TODO: Ensure you take care of nested events
+      TimeRange firstTimeRange = invalidTimeRanges.get(index);
+      TimeRange secondTimeRange = invalidTimeRanges.get(index + 1);
+
+      // Check if TimeRanges conflict, and if so, merge them.
+      if (firstTimeRange.overlaps(secondTimeRange)) {
+        // Create merged TimeRange.
+        int endMergedTime = (firstTimeRange.end() > secondTimeRange.end()) ?
+          firstTimeRange.end() : secondTimeRange.end();
+        TimeRange mergedTimeRange = TimeRange.fromStartEnd(firstTimeRange.start(), 
+          endMergedTime, false);
+
+        // Remove old TimeRanges, and add new merged TimeRange.
+        invalidTimeRanges.remove(firstTimeRange);
+        invalidTimeRanges.remove(secondTimeRange);
+        invalidTimeRanges.add(index, mergedTimeRange);
+      } else {
+        index++;
+      }
+    }
+
+    return invalidTimeRanges;
+
+  }
+
+  /**
+   * Return all of the invalid TimeRanges (conflicting events) to a list.
+   */
+  private List<TimeRange> getInvalidTimeRanges(
+    List<TimeRange> invalidTimeRanges, Collection<Event> events, 
+    Collection<String> attendees) {
+
+    // Add event TimeRange if event has attendees in current meeting query.
+    for (Event e : events) {
+      if (containsElement(attendees, e.getAttendees())) {
+        invalidTimeRanges.add(e.getWhen());
+      }
+    }
+
+    return invalidTimeRanges;
+  }
+
+  /**
+   * Return true if l1 contains any element in l2; otherwise, false.
+   */
+  private boolean containsElement(Collection<String> l1, Collection<String> l2) {
+    for (String ls2 : l2) {
+      if (l1.contains(ls2)) {
+        return true;
+      }
+    }
+    return false;
+  }
+  
 }
